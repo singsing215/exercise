510c197ae643c0564a3c34b53cc101f572d3fd50
singsing215 <felvex@163.com>
2020-03-06 20:40:14 +0800

    write csv

--- extract-top-100-movies/Rank Crawler.py
+++ extract-top-100-movies/Rank Crawler.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import pandas as pd
 
 
 movie = 'https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm' 
@@ -12,27 +12,27 @@ bs = BeautifulSoup(html.text, 'lxml') # manage tags in HTML document
 table = bs.find('tbody', {'class': 'lister-list'})
 movie_list = table.find_all('tr')
 
-#print(movie_list)
-'''
-def rename():
-    for w in rank_moving:
-        if w == '<span class="global-sprite titlemeter up"></span>':
-            w = 'up'
-        elif w == '<span class="global-sprite titlemeter down"></span>':
-            w = 'down'
-        else : w = 'unchange'
-    return w
-'''
 
+movie_name_list=[]
+release_year_list=[]
+rank_moving_list=[]
+movie_rate_list=[]
 for ml in movie_list:
-    #movie_id = ml['data-tconst']
-    #name = movie_list.find('span', {'class': 'secondaryInfo'})
     movie_name = ml.find('td', {'class': 'titleColumn'}).find('a').get_text()
-    print(movie_name)
+    movie_name_list.append(movie_name)
     release_year = ml.find('span', {'class': 'secondaryInfo'}).get_text()
-    print(release_year)
+    release_year_list.append(release_year)
     rank_moving = ml.find('span', {'class': 'global-sprite'})
-    print(rank_moving)
+    rank_moving_list.append(rank_moving)
     movie_rate = ml.find('td', {'class': 'ratingColumn imdbRating'}).get_text().strip()
-    print(movie_rate)
-#print(movie_list)
+    movie_rate_list.append(movie_rate)
+    
+
+test=pd.DataFrame({'movie name':movie_name_list,'release year':release_year_list,'rank moving':rank_moving_list,'movie rate':movie_rate_list})
+test.to_csv('C:\\Users\\SingSing\\Documents\\GitHub\\exercise\\extract-top-100-movies\\test.csv')
+
+
+
+
+
+
