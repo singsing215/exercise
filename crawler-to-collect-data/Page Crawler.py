f9e76c069e166ed6a2ee33d4f8232f7d8919f9f8
singsing215 <felvex@163.com>
2020-03-06 18:09:44 +0800

    Extract i) movie name, ii) release year, iii) rank moving , and iv) overall rating.

--- crawler-to-collect-data/Page Crawler.py
+++ crawler-to-collect-data/Page Crawler.py
@@ -7,6 +7,7 @@ movie = 'http://www.imdb.com/title/tt2527336/'
 html = requests.get(movie) # connect to the server 
 #print(html.text)
 bs = BeautifulSoup(html.text, 'lxml') # manage tags in HTML document
+
 # crawl movie description 
 desc = bs.find('div', {'class': 'summary_text'})
 movie_desc = desc.get_text().strip()
@@ -21,9 +22,13 @@ image = requests.get(img_link)
 with open('star_war_poster.jpg', 'wb') as f:
     f.write(image.content) # writing the picture
 
+#extract movie links
 recommended_movies = bs.find('div', {'class': 'rec_page'})
 movie_list = recommended_movies.find_all('div', {'class': 'rec_item'})
 for ml in movie_list:
     movie_id = ml['data-tconst']
     movie_link = 'http://www.imdb.com/title/' + movie_id + '/'
     print(movie_link)
+    
+    
+    
--- extract-top-100-movies/Rank Crawler.py
+++ extract-top-100-movies/Rank Crawler.py
@@ -9,9 +9,30 @@ html = requests.get(movie) # connect to the server
 
 bs = BeautifulSoup(html.text, 'lxml') # manage tags in HTML document
 
-# crawl movie description 
-desc = bs.find_all('td', {'class': 'titleColumn'})
-print(desc)
+table = bs.find('tbody', {'class': 'lister-list'})
+movie_list = table.find_all('tr')
 
+#print(movie_list)
+'''
+def rename():
+    for w in rank_moving:
+        if w == '<span class="global-sprite titlemeter up"></span>':
+            w = 'up'
+        elif w == '<span class="global-sprite titlemeter down"></span>':
+            w = 'down'
+        else : w = 'unchange'
+    return w
+'''
 
-
+for ml in movie_list:
+    #movie_id = ml['data-tconst']
+    #name = movie_list.find('span', {'class': 'secondaryInfo'})
+    movie_name = ml.find('td', {'class': 'titleColumn'}).find('a').get_text()
+    print(movie_name)
+    release_year = ml.find('span', {'class': 'secondaryInfo'}).get_text()
+    print(release_year)
+    rank_moving = ml.find('span', {'class': 'global-sprite'})
+    print(rank_moving)
+    movie_rate = ml.find('td', {'class': 'ratingColumn imdbRating'}).get_text().strip()
+    print(movie_rate)
+#print(movie_list)
