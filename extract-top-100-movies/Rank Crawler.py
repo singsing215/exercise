06dc73e6fc733802fb64f23ca396384ddb9a408d
singsing215 <felvex@163.com>
2020-03-06 21:20:10 +0800

    change rank_moving type and rename

--- extract-top-100-movies/Rank Crawler.py
+++ extract-top-100-movies/Rank Crawler.py
@@ -20,16 +20,32 @@ movie_rate_list=[]
 for ml in movie_list:
     movie_name = ml.find('td', {'class': 'titleColumn'}).find('a').get_text()
     movie_name_list.append(movie_name)
+    
     release_year = ml.find('span', {'class': 'secondaryInfo'}).get_text()
     release_year_list.append(release_year)
-    rank_moving = ml.find('span', {'class': 'global-sprite'})
+    
+    rank_moving = str(ml.find('span', {'class': 'global-sprite'}))
     rank_moving_list.append(rank_moving)
+    
     movie_rate = ml.find('td', {'class': 'ratingColumn imdbRating'}).get_text().strip()
     movie_rate_list.append(movie_rate)
-    
+#print(release_year_list)
 
-test=pd.DataFrame({'movie name':movie_name_list,'release year':release_year_list,'rank moving':rank_moving_list,'movie rate':movie_rate_list})
-test.to_csv('C:\\Users\\SingSing\\Documents\\GitHub\\exercise\\extract-top-100-movies\\test.csv')
+def rename(rank_moving_list):
+    a=[]
+    for w in rank_moving_list:
+        if w == '<span class="global-sprite titlemeter down"></span>':
+            a.append('down')
+        elif w == '<span class="global-sprite titlemeter up"></span>':
+            a.append('up')
+        else:
+            a.append('unchange')
+    return a
+
+#print(rename(rank_moving_list))
+
+output=pd.DataFrame({'movie name':movie_name_list,'release year':release_year_list,'rank moving':rename(rank_moving_list),'movie rate':movie_rate_list})
+output.to_csv('C:\\Users\\SingSing\\Documents\\GitHub\\exercise\\extract-top-100-movies\\output.csv')
 
 
 
