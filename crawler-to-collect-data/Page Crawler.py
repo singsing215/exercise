6ab78b112a9af6397c56b5d3c18cfa9ffba72398
singsing215 <felvex@163.com>
2020-03-06 16:59:07 +0800

    crawl the poster

--- crawler-to-collect-data/Page Crawler.py
+++ crawler-to-collect-data/Page Crawler.py
@@ -12,3 +12,12 @@ desc = bs.find('div', {'class': 'summary_text'})
 movie_desc = desc.get_text().strip()
 print(movie_desc)
 
+# crawl the poster
+poster = bs.find('div', {'class': 'poster'})
+img = poster.find('img')
+img_link = img['src'] # access the value of an attribute of a tag
+print(img_link)
+image = requests.get(img_link)
+with open('star_war_poster.jpg', 'wb') as f:
+    f.write(image.content) # writing the picture
+
