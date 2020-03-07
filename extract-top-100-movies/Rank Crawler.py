7203883d80fd83f600f4459e2b5b9b2917068e62
singsing215 <felvex@163.com>
2020-03-07 14:53:01 +0800

    Extract all the summary text in new pages

--- extract-top-100-movies/Rank Crawler.py
+++ extract-top-100-movies/Rank Crawler.py
@@ -17,6 +17,7 @@ movie_name_list=[]
 release_year_list=[]
 rank_moving_list=[]
 movie_rate_list=[]
+movie_desc_list=[]
 for ml in movie_list:
     movie_name = ml.find('td', {'class': 'titleColumn'}).find('a').get_text()
     movie_name_list.append(movie_name)
@@ -29,6 +30,17 @@ for ml in movie_list:
     
     movie_rate = ml.find('td', {'class': 'ratingColumn imdbRating'}).get_text().strip()
     movie_rate_list.append(movie_rate)
+    
+    movie_id= ml.find('td', {'class': 'titleColumn'}).find('a')['href']
+    #print(movie_id)
+    movie_link = 'http://www.imdb.com' + movie_id 
+    print(movie_link)
+    new = requests.get(movie_link)
+    bs = BeautifulSoup(new.text, 'lxml')
+    desc = bs.find('div', {'class': 'summary_text'})
+    movie_desc = desc.get_text().strip()
+    print(movie_desc)
+    movie_desc_list.append(movie_desc)
     
 #print(release_year_list)
 
@@ -45,9 +57,10 @@ def rename(rank_moving_list):
 
 #print(rename(rank_moving_list))
 
-output=pd.DataFrame({'movie name':movie_name_list,'release year':release_year_list,'rank moving':rename(rank_moving_list),'movie rate':movie_rate_list})
-output.to_csv('C:\\Users\\SingSing\\Documents\\GitHub\\exercise\\extract-top-100-movies\\output.csv')
 
+output=pd.DataFrame({'movie name':movie_name_list,'release year':release_year_list,'rank moving':rename(rank_moving_list),'movie rate':movie_rate_list,'movie desc':movie_desc_list})
+output.to_csv('C:\\Users\\SingSing\\Documents\\GitHub\\exercise\\extract-top-100-movies\\output.csv')
+print('File Output Success')
 
 
 
