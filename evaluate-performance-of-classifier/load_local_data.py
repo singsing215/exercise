65cac081611d52b2117ec091f0f3fe0aea241d66
singsing215 <felvex@163.com>
2020-03-08 16:59:36 +0800

    remove

--- evaluate-performance-of-classifier/load_local_data.py
+++ evaluate-performance-of-classifier/load_local_data.py
@@ -1,9 +1,4 @@
 from twenty_newsgroups import load_20newsgroups
-import re
-import os
-import math
-import pandas as pd
-from pandas import ExcelWriter
 
 # The reference of the data set 20 Newsgroups can be found here:
 # http://qwone.com/~jason/20Newsgroups/
@@ -25,7 +20,7 @@ print(train_data.target_names[0])#alt.atheism
 print(train_data.target_names[1])#comp.sys.mac.hardware
 print(train_data.target_names[2])#rec.sport.baseball
 print(train_data.target_names[3])#sci.med
-
+'''
 
 doc_name_list = []
 for i in range(2249):
@@ -37,56 +32,6 @@ for i in range(2249):
     print('')
     print(train_data.data[i])
     print('')
-'''
-
-def term_freq(word_list):  # words in a document
-    word_dict = {}
-    for w in word_list:
-        if w in word_dict:
-            word_dict[w] += 1
-        else:
-            word_dict[w] = 1
-    # calculate #words in total and then calculate the frequency
-    word_num = sum(word_dict.values())
-    for w in word_dict.keys():
-        word_dict[w] /= word_num
-    return word_dict
-
-
-def inv_doc_freq(term_set, doc_name2word_list):
-    doc_num = len(doc_name2word_list)
-    idf_dict = {}
-    # term in all doc
-    for w in term_set:
-        doc_count = 0
-        # find the appearing frenquency among all documents
-        for word_list in doc_name2word_list.values():
-            if w in word_list:
-                doc_count += 1
-        idf_dict[w] = math.log(doc_num / doc_count)
-    return idf_dict
-
-
-if __name__ == '__main__':
-    doc_name2word_list = {}
-    doc_name2tf_dict = {}
-    term_set = set()
-    '''read all txt files name from directory rootdir
-    All the txt files should be put under the same directory as this python file'''
-    #rootdir = os.path.dirname(os.path.abspath(__file__))
-    doc_name_list = [item for item in train_data.filenames]
-    #print(doc_name_list)
-    content_list = [line for line in train_data.data]
-    content = str(content_list)
-    content = content.strip()
-    content = re.sub('[^A-Za-z\s]', ' ', content)
-    content = content.lower()
-    word_list = content.split()
-    doc_name2word_list = set(word_list)
-    doc_name2tf_dict = term_freq(word_list)
-    print(doc_name2tf_dict)
-
-
 
 
 
